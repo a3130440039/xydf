079ddfd2f50a1c858d0f2880ef56481b5cb38b6e
payjuhe <jihuiling520@gmail.com>
2020-12-19 10:34:23 +0800

    提交SQL

--- Start.sh
+++ Start.sh
@@ -4,4 +4,4 @@ nohup bash OnPayFiveMinute.sh > /dev/null &
 nohup bash OnPayThreeMinute.sh > /dev/null &
 nohup bash OnPayTwoMinute.sh > /dev/null &
 nohup bash OnPayOneMinute.sh > /dev/null &
-nohup bash Notify.sh > /dev/null &
+nohup bash Notify.sh > /dev/null  &  
